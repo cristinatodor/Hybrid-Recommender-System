11331c6d496f1c74a7f17ef55f3b633f063f359d
Cristina Todor <44347444+cristinatodor@users.noreply.github.com>
2021-02-01 09:35:04 +0200

    Cleaned content_based, hybrid

--- content_based.py
+++ content_based.py
@@ -1,5 +1,6 @@
-# source code
+# code based on 
 # https://github.com/youonf/recommendation_system/blob/master/content_based_filtering/content_based_recommender_approach2_v2.ipynb
+
 import os
 import collections
 import csv
@@ -17,28 +18,23 @@ from nltk.corpus import stopwords
 
 from rake_nltk import Rake 
 import nltk 
-#nltk.download('stopwords')
+
 
 def prep_data(delivery):
     df = pd.read_csv('./data/2019_3months_Restaurants_reviews.csv')
     df = df.drop(['cool', 'funny', 'useful', 'date', 'count', 'review_id'], axis = 1)
-    print(df.shape)
-
-    df_selected_business = pd.read_csv('./data/selected_business_Restaurants_Toronto.csv')
-    df_selected_business = df_selected_business[df_selected_business['business_id'].isin(df['business_id'])]
 
     # businesses that aren't temporary closed
     df_selected_business_covid = pd.read_csv('./data/selected_business_Restaurants_Toronto_covid.csv')
     df_selected_business_covid = df_selected_business_covid[df_selected_business_covid['business_id'].isin(df['business_id'])]
-    print('Selected business covid\n', df_selected_business_covid.columns)
 
-    print('\n')
     df_selected_business_covid = df_selected_business_covid.drop(['Unnamed: 0', 'Temporary Closed Until', 'Covid Banner', 'Virtual Services Offered'], axis=1)
     
     if delivery:
         df_selected_business_covid = df_selected_business_covid[df_selected_business_covid['delivery or takeout'] == True]
-    print('Selected business covid shape: ', df_selected_business_covid.shape)
-    print(df_selected_business_covid.columns)
+
+    # only keep businesses that have at least 20 ratings
+    df_selected_business_covid = df_selected_business_covid[df_selected_business_covid['review_count'] >= 20]
 
     df = df[df['business_id'].isin(df_selected_business_covid['business_id'])]
 
@@ -48,39 +44,19 @@ def prep_data(delivery):
     df = df.drop(index=4322)
     print(df.loc[11400])
     df = df.drop(index=11400)
-    '''
-
     #print(df.loc[11740])
     #df = df.drop(index=11740) 
+    '''
 
     unique_business_id = df['business_id'].unique()
     unique_user_id = df['user_id'].unique()
-    print('Unique businesses: ', len(unique_business_id))
-    print('Unique users: ', len(unique_user_id))
 
     ratings = df[['user_id', 'stars', 'business_id', 'name', 'categories']]
 
-    '''
-    df_selected_business_tip = pd.read_csv('./data/selected_business_CoffeeTea_tip.csv')
-    print(df_selected_business_tip.head(10))
-    print('Df business tip: ', df_selected_business_tip.shape)
-    df_merged = pd.merge(df_selected_business, df_selected_business_tip, how = 'left', left_on='business_id', right_on = 'business_id')
-    df_merged = df_merged.replace(np.nan, '', regex=True)
-    #df_merged = df_merged['text'].fillna('')
-    print(df_merged.head(10))
-    print('Df business: ', df_merged.shape)
-    '''
-
-    print('\n')
-    print(df_selected_business_covid.head(20))
-    print('Df business: ', df_selected_business_covid.shape)
-
     df_merged = df_selected_business_covid
-    print('\n\n')
 
     unique_business_id = df_merged['business_id'].unique()
     business_shape = unique_business_id.shape
-    #print('Number of Unique Business ID: %d' % business_shape[0])
     business_df = pd.DataFrame({
             'business_id' :unique_business_id,
             'business_index': range(business_shape[0])
@@ -88,23 +64,13 @@ def prep_data(delivery):
 
     df_merged = pd.merge(df_merged, business_df, how = 'inner', left_on='business_id', right_on = 'business_id')
 
-    #df_merged = df_merged.drop(['business_id', 'avg_stars', 'review_count'], axis=1)
+    
     df_merged = df_merged.drop(['avg_stars', 'review_count'], axis=1)
-    #print(df_selected_business.head(10))
 
-    contents_df = df_merged[['business_id', 'name', 'city', 'categories', 'attributes']] #'text'
-    #contents_df = contents_df.drop(['business_index'], axis=1) # check if needeed 
-    print(df_selected_business.head(10))
-
+    contents_df = df_merged[['business_id', 'name', 'city', 'categories', 'attributes']] 
 
     contents_df['categories'] = contents_df['categories'].map(lambda x: x.lower().split(','))
-    #unique_df['categories'] = unique_df['categories'].map(lambda x: x.split(','))
-    #contents_df = df_selected_business.sort_values(by='business_index')
 
-    '''
-    categories = unique_df['categories']
-    print(categories.head(10))
-    '''
     copy_contents_df = contents_df.copy()
 
     def remove_spaces(cat_list):
@@ -123,10 +89,8 @@ def prep_data(delivery):
             print(copy_unique_df.iloc[index]['categories'])
         '''        
         x_list = remove_spaces(row['categories'])
-        #print(x_list)
-        #copy_unique_df.iloc[index]['categories'] = x_list
+       
         copy_contents_df.at[index, 'categories'] = x_list
-        #print(copy_unique_df.iloc[index]['categories'])
 
         x = ''.join(row['city'].split())
         copy_contents_df.at[index, 'city'] = x.lower()
@@ -135,44 +99,23 @@ def prep_data(delivery):
     contents_df['categories'] = copy_contents_df['categories']
     contents_df['city'] = copy_contents_df['city']
 
-    contents_df = contents_df.drop(['attributes'], axis=1)# change, neeed to work with attributes as well 
-    #print(contents_df.head(10))
-    #print(contents_df.shape)
-
+    contents_df = contents_df.drop(['attributes'], axis=1)
+   
     # item profile 
     df_item = contents_df[['business_id', 'categories']]
-    print('Item profile \n', df_item.head())
-
-    '''
-    # one-hot encoding for category
-    #df_category = pd.get_dummies(df_item['categories'])
-    #df_categories = pd.DataFrame(df_item['categories'].to_list())
-    df_categories = df_item.explode('categories')
-    print(df_categories.head(10))
-    print(df_categories.shape)
-
-    unique_category = df_categories['categories'].unique()
-    print(unique_category.shape)
-    '''
 
     categories = df_item['categories']
     df_categories = df_item.explode('categories')
-    print('Categories: \n', categories)
-    #print(df_categories.head(10))
 
     # one-hot encoding for category
     df_oh_categories = pd.get_dummies(df_categories['categories'])
     #normalized
     df_categories_normalized = df_oh_categories.apply(lambda x: x/np.sqrt(df_oh_categories.sum(axis=1)))
-    #df_categories_normalized = df_categories_normalized.drop_duplicates()
-    #print(df_categories_normalized)
 
     #create item profile
     df_item = pd.concat([df_item, df_categories_normalized], axis=1)
     df_item = df_item.drop(columns='categories')
-    #df_item = df_item.drop_duplicates()
 
-    #df_item_unique = df_item.groupby(df_item.index).sum()
     df_item_unique = df_item.groupby('business_id').sum()
 
     return ratings, df_item_unique, contents_df, unique_business_id
@@ -183,13 +126,15 @@ def recommend_cbf(user_id, delivery):
     ratings, df_item_unique, contents_df, unique_business_id = prep_data(delivery)
 
     rating_df = pd.pivot_table(ratings, values='stars', index=['business_id'], columns = ['user_id'])
-    print('Ratings: ', rating_df.shape)
-    #print(rating_df.head(10))
 
     # get number of users
     users_no = rating_df.columns
 
     df_users = pd.DataFrame(columns=df_item_unique.columns)
+
+    # normally, a matrix containing all users is built
+    # because this takes a while, only the row for the active user is computed here
+    # this doesn't affect the recommender, as it builds the user profile based only on the active user's preferences 
 
     '''
     for i in tqdm(range(len(users_no))):
@@ -199,15 +144,11 @@ def recommend_cbf(user_id, delivery):
     '''
     user_list = users_no.tolist()
     index = user_list.index(user_id)
-    print('Index of user_id: ', index)
-    print('User no: ', users_no[index])
 
     #i = 0 
     working_df = df_item_unique.mul(rating_df.iloc[:,index], axis=0)
     working_df.replace(0, np.NaN, inplace=True)    
     df_users.loc[users_no[index]] = working_df.mean(axis=0)
-    print('Df users: \n', df_users.head())
-
 
     document_frequency = df_item_unique.sum()
     idf = (len(unique_business_id)/document_frequency).apply(np.log) #log inverse of DF
@@ -226,7 +167,6 @@ def recommend_cbf(user_id, delivery):
     #i = 0
     working_df = idf_df_item.mul(df_users.iloc[0], axis=1)
     df_predict[users_no[index]] = working_df.sum(axis=1)
-    print('Predict shape: ', df_predict.shape)
 
     # get all business id
     business_id = df_predict.index
@@ -234,41 +174,17 @@ def recommend_cbf(user_id, delivery):
     #user predicted rating to all businesses
     user_predicted_rating = df_predict[user_id]
 
-    #user_predicted_rating.sort_values(ascending=False)
-
-    print(user_predicted_rating)
-
     # combine ratings and business details
     user_rating_business = pd.concat([user_predicted_rating, contents_df.set_index('business_id')], axis=1)
 
-    print('User rating busienss', user_rating_business)
+    already_rated = ratings[ratings['user_id'].isin([user_id])]
 
-    #places already rated by user
-    already_rated = ratings[ratings['user_id'].isin([user_id])]#['business_id']
-
-    print('User has already rated: \n', already_rated)
-
-    #recommendation without placesd being rated by user
+    #recommendation without places being rated by user
     all_rec = user_rating_business[~user_rating_business.index.isin(already_rated['business_id'])]
-    print('\nAll rec shape: ', all_rec.shape)
 
-    return all_rec.sort_values(by=[user_id], ascending=False)#.iloc[0:5704]
-    
-test_user_id = 'TUMlJiM6Aw-xogVcF876qw'
-test_user_id = 'zy1M09juuXCmAnpbWRqDhQ'
-test_user_id = '--BumyUHiO_7YsHurb9Hkw'
-
-user_id =  '--7gjElmOrthETJ8XqzMBw'
-
-test_user_id1 = '--7gjElmOrthETJ8XqzMBw'
-#cb = recommend_cbf(test_user_id1, delivery=True)
-#print(cb)
+    return all_rec.sort_values(by=[user_id], ascending=False)
+ 
 
 
 
-'''
-vectorizer = TfidfVectorizer(stop_words='english', max_features=2000)
-vectors = vectorizer.fit_transform(categories).toarray()
-words = vectorizer.get_feature_names()
-print(vectors.shape)
-'''
+
--- hybrid.py
+++ hybrid.py
@@ -1,4 +1,3 @@
-
 import os
 import collections
 import csv
@@ -10,34 +9,29 @@ import pandas as pd
 from scipy.linalg import sqrtm
 from scipy.sparse.linalg import svds
 
-#from content_based_v3 import recommend_cosine
-#from content_based_v2_copy import recommend
-#from collaborative_v2 import make_predictions, recommend_places
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+import math
 
 from content_based import recommend_cbf
 from collaborative import recommend_cf
 
-def main():
-    '''
-    df = pd.read_csv('./data/last_1_year_Restaurants_reviews.csv')
-    df = df.drop(['useful', 'funny', 'cool', 'date'], axis = 1)
-    df_selected_business = pd.read_csv('./data/selected_business_Restaurants_Toronto.csv')
-    df_selected_business = df_selected_business[df_selected_business['business_id'].isin(df['business_id'])]
-    '''
+from texttable import Texttable
 
+def main():
     df = pd.read_csv('./data/2019_3months_Restaurants_reviews.csv')
     df = df.drop(['cool', 'funny', 'useful', 'date', 'count', 'review_id'], axis = 1)
 
-    #df_selected_business = pd.read_csv('./data/selected_business_Restaurants_Toronto.csv')
-    #df_selected_business = df_selected_business[df_selected_business['business_id'].isin(df['business_id'])]
-
-    # businesses that aren't temporary closed
+    # businesses that aren't temporary closed 
     df_selected_business_covid = pd.read_csv('./data/selected_business_Restaurants_Toronto_covid.csv')
     df_selected_business_covid = df_selected_business_covid[df_selected_business_covid['business_id'].isin(df['business_id'])]
 
     print('\n')
     df_selected_business_covid = df_selected_business_covid.drop(['Unnamed: 0', 'Temporary Closed Until', 'Covid Banner', 'Virtual Services Offered'], axis=1)
 
+    print('Welcome to Toronto restaurant recommender!\n')
     delivery_ans = input('Do you want delivery/takeaway?[Y/N]')
 
     if delivery_ans == 'y' or delivery_ans == 'Y':
@@ -47,14 +41,13 @@ def main():
 
     if delivery:
         df_selected_business_covid = df_selected_business_covid[df_selected_business_covid['delivery or takeout'] == True]
-
-    print('Selected business covid shape: ', df_selected_business_covid.shape)
-    print(df_selected_business_covid.columns)
+    
+    # only keep businesses that have at least 20 ratings
+    df_selected_business_covid = df_selected_business_covid[df_selected_business_covid['review_count'] >= 20]
 
     df = df[df['business_id'].isin(df_selected_business_covid['business_id'])]
 
     df_delivery = df_selected_business_covid[df_selected_business_covid['delivery or takeout'] == True]
-    df_no_delivery = df_selected_business_covid[df_selected_business_covid['delivery or takeout'] == False]
  
     # only for testing purposes
     '''
@@ -62,18 +55,14 @@ def main():
     df = df.drop(index=4322)
     print(df.loc[11400])
     df = df.drop(index=11400)
-    '''
-
     #print(df.loc[11740])
     #df = df.drop(index=11740) 
+    '''
     
-    #print(df.loc[2783])
-    #df = df.drop(index=2783)
-
     # reconstruct business id
     unique_business_id = df['business_id'].unique()
     business_shape = unique_business_id.shape
-    print('There are: %d businesses' % business_shape[0])
+    print('\nThere are: %d businesses' % business_shape[0])
 
     business_df = pd.DataFrame({
             'business_id' :unique_business_id,
@@ -82,15 +71,11 @@ def main():
 
     business_dict = pd.Series(business_df.business_id, index = business_df.business_index)
 
-    #business_index = int(input('Please enter a business index: '))
-    #business_id = business_dict[business_index]
-    #print('The corresponding business id is: %s' % business_id)
 
-    print('\n')
     # reconstruct user id
     unique_users_id = df['user_id'].unique()
     user_shape = unique_users_id.shape
-    print('There are: %d users' % user_shape[0])
+    print('\nThere are: %d active users' % user_shape[0])
 
     user_df = pd.DataFrame({
             'user_id': unique_users_id,
@@ -99,39 +84,24 @@ def main():
 
     user_dict = pd.Series(user_df.user_id, index = user_df.user_index)
 
-    user_index = int(input('Please enter a user index: '))
+    user_index = int(input('\nPlease enter a user index (number): '))
     user_id = user_dict[user_index]
-    print('The corresponding user id is: %s' % user_id)
+    print('The corresponding user id is: %s' % user_id, '\n')
 
-    '''
-    test_user_id =  '--7gjElmOrthETJ8XqzMBw'
-    test_user_id = '--BumyUHiO_7YsHurb9Hkw'
-    test_user_id = 'ltvHnOfno4e5wUdmMDU72A'
-    '''
     # used for measuring accuracy of usage predictions
-    #test_user_id = 'jI43jHCXx1R-gg_vYcQWww'
+    test_user_id = 'jI43jHCXx1R-gg_vYcQWww'
     #user_id = test_user_id
 
     # content-based recommendations
     cbf = recommend_cbf(user_id, delivery)
     
     # collaborative filtering recommendations
-    #recommend_places(preds_df, test_user_id1, df, cb, 25) this keeps all the places recommedend bf CBF, ordered by prediction 4
-    cf = recommend_cf(user_id, delivery)
+    cf = recommend_cf(user_id, delivery, num_recs=len(business_dict))
 
-    print('\nHybrid')
-    print('\nContent_based: \n', cbf)
-    print('\n')
-    print('Collaborative: \n', cf)
-
-    #print(cb.index)
-    
-    num_items = 3655
+    print('\n') 
 
     cbf_index_list = cbf.index.tolist()
     cf_index_list = cf['business_id'].tolist()
-    #print('Cb index list: \n', cb_index_list)
-    #print('Cf index list: \n', cf_index_list)
 
     cbf_ranking_dict = {}
     cf_ranking_dict = {}
@@ -140,120 +110,163 @@ def main():
     for i in range(len(cbf_index_list)):
         cbf_ranking_dict[cbf_index_list[i]] = i + 1
     
-    print('Length of content-based dict: ', len(cbf_ranking_dict))
     
     for i in range(len(cf_index_list)):
         cf_ranking_dict[cf_index_list[i]] = i + 1
 
-    print('Length of collaborative dict: ', len(cf_ranking_dict))
+
+    num_items = len(cf_ranking_dict)
 
     score_dict = {}
     
+    # compute hybrid score 
     for business_id in cf_ranking_dict:
         score = 1 + num_items - cf_ranking_dict[business_id]
-        #print(business_id, score, '\n')
 
         if business_id in cbf_ranking_dict:
-            #print('yes', business_id)
             score = score * (1 + num_items - cbf_ranking_dict[business_id])
         
         score_dict[business_id] = score
     
-    #print('Score dict:', score_dict)
-    #sorted_dict = dict(sorted(score_dict.items(), key=lambda item: item[1]))
     sorted_dict = dict(sorted(score_dict.items(), key=operator.itemgetter(1),reverse=True))
-    print('Sorted dict len:', len(sorted_dict))
-    #print(sorted_dict)
-
-    #n_items = take(20, sorted_dict.iteritems())
+ 
     n_items = list(itertools.islice(sorted_dict.items(), 20))
-    print(n_items)
 
     business_ids = []
     for pair in n_items:
         business_ids.append(pair[0])
-    
-    print('\nBusiness ids:', business_ids)
 
-    print(df_selected_business_covid.shape)
-    print('\nBusiness_details\n')
 
     recommendations = pd.DataFrame()
+    
+    
+    sum_log = 0
 
-    i = 1
     for business_id in business_ids:
         recommendation = df_selected_business_covid[df_selected_business_covid['business_id'] == business_id]
         recommendations = recommendations.append(recommendation)
-        '''
-        business_name = recommendation['name']
-        city = recommendation['city']
-        avg_stars = recommendation['avg_stars']
-        categories = recommendation['categories']
-        review_count = recommendation['review_count']
-        delivery = recommendation['delivery or takeout']
 
-        print(i)
-        print(business_name)
-        print('City', city)
-        print('Avg_stars', avg_stars)
-        print('Categories', categories)
-        print('Reviews:', review_count)
-        print('Delivery or takeout', delivery)
-        print('\n')
-        i += 1
-        '''
-
-    print(recommendations)
-
-    recommendations_show = recommendations.drop(['business_id'], axis=1)
-    recommendations_show = recommendations_show[['name', 'city', 'avg_stars', 'categories', 'review_count', 'attributes', 'delivery or takeout']]
-    print(recommendations_show)
-
-    
+        #sum_log += math.log(recommendation['review_count'], 2) # used for testing novelty 
+       
+    # testing novelty
     '''
-    top_all = dict(itertools.islice(sorted_dict.items(), 60))
-    all_business_ids = top_all.keys()
-    all_recommendations = df_selected_business_covid[df_selected_business_covid['business_id'].isin(all_business_ids)]
-    print(all_recommendations.shape)
-    print('All rec top 20:\n', all_recommendations)
-    
-    #top20 = OrderedDict(itertools.islice(sorted_dict.iteritems(), 20))
-    top20 = dict(itertools.islice(sorted_dict.items(), 20))
-    print('\n', top20)
-
-    print(df_selected_business_covid.shape)
-    print('\nBusiness_details\n')
-
-    business_ids = top20.keys()
-    #details_df = df.drop_duplicates()
-    recommendations = df_selected_business_covid[df_selected_business_covid['business_id'].isin(business_ids)]
-    print(recommendations.columns)
-
-    recommendations_show = recommendations.drop(['business_id'], axis=1)
-    print(recommendations_show)
+    novelty = -1 * sum_log / len(business_ids)
+    print('Novelty: ', novelty, '\n')
     '''
-    
-    '''
-    for business_id in top500:
-        print(df_selected_business_covid[df_selected_business_covid['business_id'] == business_id][['name', 'categories', 'avg_stars']])
-        print('\n')
-    '''
+
     user_rated = df[df['user_id'] == user_id]
-    #user_rated = user_rated.drop(['review_count', 'review_id', 'text', 'count'], axis = 1)
-    user_rated = user_rated[['business_id', 'name', 'categories', 'text']]
+    user_rated = user_rated[['business_id', 'name', 'categories', 'text', 'stars']]
     user_rated = user_rated.drop_duplicates()
 
-    print('User has already rated: {0} places'.format(user_rated.shape[0]))
-    print(user_rated)
+    places_rated = list(user_rated['name'])
+    categories_rated = list(user_rated['categories'])
+    text_rated = list(user_rated['text'])
+    stars_rated = list(user_rated['stars'])
 
+    print('User {0} has already rated: {1} place(s)'.format(user_index, user_rated.shape[0]))
+
+    rated_table = Texttable()
+    rated_table_text = Texttable()
+    rated_table.add_rows([['Restaurant', 'Categories', 'Stars']])
+    rated_table_text.add_rows([['Restaurant', 'Categories', 'Text']])
+
+    rated_table_text.set_cols_align(["c", "c", "c"])
+    rated_table_text.set_cols_width([15, 40, 120])
+
+    for i in range(len(places_rated)):
+        rated_table.add_row([places_rated[i], categories_rated[i], stars_rated[i]])
+        rated_table_text.add_row([places_rated[i], categories_rated[i], text_rated[i]])
+
+    print(rated_table.draw())
+
+    recs1 = recommendations.iloc[:10, :]
+    recs2 = recommendations.iloc[10:, :]
+   
+
+    print('\nRecommendations')
+    rec_table = Texttable()
+    rec_table.add_rows([['', 'Restaurant', 'Avg stars', 'Categories', 'Reviews', 'Delivery/Takeout']])
+
+    rec_table.set_cols_align([ "c", "c", "c", "c", "c", "c"])
+    rec_table.set_cols_width([5, 15, 9, 40, 7, 9])
+
+    name_rec1 = list(recs1['name'])
+    stars_rec1 = list(recs1['avg_stars'])
+    categories_rec1 = list(recs1['categories'])
+    reviews_rec1 = list(recs1['review_count'])
+    attrib_rec1 = list(recs1['attributes'])
+    delivery_rec1 = list(recs1['delivery or takeout'])
+
+    name_rec2 = list(recs2['name'])
+    stars_rec2 = list(recs2['avg_stars'])
+    categories_rec2 = list(recs2['categories'])
+    reviews_rec2 = list(recs2['review_count'])
+    attrib_rec2 = list(recs2['attributes'])
+    delivery_rec2 = list(recs2['delivery or takeout'])
+
+    for i in range(len(name_rec1)):
+        rec_table.add_row([i + 1, name_rec1[i], stars_rec1[i], categories_rec1[i], reviews_rec1[i], delivery_rec1[i]])
+
+    print(rec_table.draw())
+
+    more_rec = input('\nWould you like to see more recommendations?[Y/N]')
+    
+    if more_rec == 'y' or more_rec == 'Y':
+        rec_table = Texttable()
+        rec_table.add_rows([['', 'Restaurant', 'Avg stars', 'Categories', 'Reviews', 'Delivery/Takeout']])
+
+        rec_table.set_cols_align([ "c", "c", "c", "c", "c", "c"])
+        rec_table.set_cols_width([5, 15, 9, 40, 7, 9])
+
+        for i in range(len(name_rec1)):
+            rec_table.add_row([i + 11, name_rec2[i], stars_rec2[i], categories_rec2[i], reviews_rec2[i], delivery_rec2[i]])
+
+        print(rec_table.draw())
+
+
+    # testing diversity
     '''
-    indexes = [4322, 11400] #,11740]   
-    business_ids = ['LBQD0H2109oltJNF1raLWA', 't7psd5uRy5NpadoONmTY7w']#, 'uZPU3Vgo7q72EKYuT-bRTQ']
-    names = ['The Haam', 'Tractor Foods']#, 'LOV King West']
+    cosine_sim, inddict = compute_cosine(df_selected_business_covid)
+
+    id1 = inddict[test_business_id1]
+    id2 = inddict[test_business_id2]
+
+    print(cosine_sim)
+    print(business_dict)
+
+    business_dict_reverse = {y:x for x, y in business_dict.items()}
+
+    print('\n')
+    ext_sum = 0
+    int_sum = 0
+
+    for i in business_ids:
+        id1 = business_dict_reverse[i]
+
+        for j in business_ids:
+            id2 = business_dict_reverse[j]
+            #if i != j:
+            int_sum += cosine_sim[id1][id2]
+        
+        ext_sum += (1 - int_sum)
+    
+  
+    n = len(business_ids)
+    print(n)
+    diversity = ext_sum / ((n / 2) * (n-1))
+
+    print('Diversity', diversity)
+    '''
+
+    # testing precision, recall
+    '''
+    indexes = [4322, 11400] #, 11740]   
+    business_ids = ['LBQD0H2109oltJNF1raLWA', 't7psd5uRy5NpadoONmTY7w'] #, 'uZPU3Vgo7q72EKYuT-bRTQ']
+    names = ['The Haam', 'Tractor Foods'] #, 'LOV King West']
 
     rec_list = list(recommendations['business_id'])
-    print('\n\n', rec_list)
-    print(len(rec_list))
+    #print('\n\n', rec_list)
+    print('Recommendations len:', len(rec_list))
 
     tp = 0
     for x in business_ids:
@@ -264,26 +277,37 @@ def main():
             print('no')
     print('TP:', tp)
     '''
-       
+
+ 
+def compute_cosine(df):
+    places = df
+    places = places.sort_values(by=['business_id'])
+    #print('\n',places.shape)
+
+    vectorizer = TfidfVectorizer(stop_words='english', analyzer='word')
+
+    # build business name tfidf matrix 
+    tfidf_matrix = vectorizer.fit_transform(places['name'])
+    tfidf_feature_name = vectorizer.get_feature_names()
+    #print(tfidf_matrix.shape)
+
+    places['all_content'] = places['name'] + places['categories'] #+ places_10['city'] 
+    tfidf_all_content = vectorizer.fit_transform(places['all_content'])
+    #print(tfidf_all_content.shape)
+
+    # computing cosine similarity matrix using linear_kernal of sklearn
+    cosine_similarity_all_content = linear_kernel(tfidf_all_content, tfidf_all_content)
+    #print('Cosine sim', cosine_similarity_all_content.shape)
+
+    indices_n = pd.Series(places['business_id'])
+    print(indices_n)
+    inddict = indices_n.to_dict()
+    inddict = dict((v,k) for k,v in inddict.items())
+    #print(places['business_id'])#
 
 
-'''
-test_user_id1 = 'TqSZTCaIE7Rmnia_4X4iIA'
-test_user_id2 = 'TUMlJiM6Aw-xogVcF876qw'
-test_user_id3 = 'ltvHnOfno4e5wUdmMDU72A'
+    return cosine_similarity_all_content, inddict
 
-test_business_id1 = 'K5Q2vkF5UpytV9Q1rB-5Yg'
-
-cb = recommend_cosine(test_business_id1)
-cb.drop(['all_content'], axis = 1)
-print(cb)
-print(cb.columns)
-
-preds_df = make_predictions()
-#recommend_places(preds_df, test_user_id1, df, cb, 25) this keeps all the places recommedend bf CBF, ordered by prediction 
-cbf = recommend_places(preds_df, test_user_id1, df, cb, 10)
-print(cbf)
-'''
 
 if __name__ == "__main__":
     main()
